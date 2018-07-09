58b4bf0a5d4f44100adafa67c136965559961afd
Ankit Srivastava -X (ansriva3 - PERSISTENT SYSTEMS INC at Cisco) <ansriva3@partnet.cisco.com>
2018-07-09 13:39:36 +0530

    added connect and disconnect handlers

--- server/server.js
+++ server/server.js
@@ -1,14 +1,28 @@
 const path=require('path');//built in path module t
 const express=require('express');
+const http=require('http');
+const socketIO=require('socket.io');
+
+
 const publicPath=path.join(__dirname,'../public')
+
 const port=process.env.port || 3000
 var app=express();
+var server=http.createServer(app);
+var io=socketIO(server);
 console.log(__dirname,'../public');
 console.log(publicPath);
 
 
 app.use(express.static(publicPath));
 
-app.listen(port,()=>{
+io.on('connection',(socket)=>{
+   console.log('new user connected');
+   socket.on('disconnect',()=>{
+       console.log('user disconnected');
+   })
+})
+
+server.listen(port,()=>{
     console.log(`server is running on port ${port} `);
 });
