43c0dbe16aa910adaa8285b8560759f65f243931
Ankit Srivastava -X (ansriva3 - PERSISTENT SYSTEMS INC at Cisco) <ansriva3@partnet.cisco.com>
2018-07-11 21:52:00 +0530

    send messages to only people in same room

--- public/js/chat.js
+++ public/js/chat.js
@@ -70,7 +70,6 @@ jQuery('#message_form').on('submit',function(e){
     e.preventDefault();
     var messageTextBox=jQuery('[name=message]');
     socket.emit('createMessage',{
-        from:'User',
         text:messageTextBox.val()
     },function(){
        messageTextBox.val('')
--- server/server.js
+++ server/server.js
@@ -39,15 +39,21 @@ io.on('connection',(socket)=>{
    
 
    socket.on('createMessage',(message,callback)=>{
-       console.log('got new message from client',message)
        //for broadcasting
-       io.emit('newMessage',generateMessage( message.from,message.text))
+       var user=users.getUser(socket.id);
+       if(user && isRealString(message.text)){
+        io.to(user.room).emit('newMessage',generateMessage( user.name,message.text))
+       }
      
        callback(); 
    
    })
    socket.on('createLocationMessage',(coords)=>{
-       io.emit('newLocationMessage',generateLocationMessage('Admin',coords.latitude ,coords.longitude))
+       var user=users.getUser(socket.id)
+       if(user){
+        io.to(user.room).emit('newLocationMessage',generateLocationMessage(user.name,coords.latitude ,coords.longitude))
+
+       }
    })
    socket.on('disconnect',()=>{
        console.log('user disconnected');
