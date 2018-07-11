9cf76c87ee914165e55fcb1a1c0c5dec9b1ad006
Ankit Srivastava -X (ansriva3 - PERSISTENT SYSTEMS INC at Cisco) <ansriva3@partnet.cisco.com>
2018-07-11 21:38:19 +0530

    add user class and update user list

--- server/server.js
+++ server/server.js
@@ -2,13 +2,16 @@ const path=require('path');//built in path module t
 const express=require('express');
 const http=require('http');
 const socketIO=require('socket.io');
+var {generateMessage}=require('./Utils/message');
+var {generateLocationMessage}=require('./Utils/message');
+var {isRealString}=require('./Utils/validation');
+const {Users}=require('./Utils/users');
 const publicPath=path.join(__dirname,'../public')
 const port=process.env.port || 3000
 var app=express();
 var server=http.createServer(app);
 var io=socketIO(server);
-var {generateMessage}=require('./Utils/message');
-var {generateLocationMessage}=require('./Utils/message');
+var users=new Users();
 
 console.log(__dirname,'../public');
 console.log(publicPath);
@@ -18,13 +21,28 @@ app.use(express.static(publicPath));
 
 io.on('connection',(socket)=>{
    console.log('new user connected');
-   socket.emit('newMessage',generateMessage( "Admin","Welcome to chat app"));
-   socket.broadcast.emit('newMessage',generateMessage( "Admin","new user joined"));
  
+ 
+   socket.on('join',(params,callback)=>{
+        if(!isRealString(params.name) || !isRealString(params.room) ){
+            return callback('Name and Room Field are required')
+        }
+        socket.join(params.room);
+        users.removeUser(socket.id)
+        users.addUser(socket.id,params.name,params.room)
+
+        io.to(params.room).emit('updateUserList',users.getUserList(params.room))
+        socket.emit('newMessage',generateMessage( "Admin","Welcome to chat app"));
+        socket.broadcast.to(params.room).emit('newMessage',generateMessage( "Admin",`${params.name} has joined`));
+        callback();
+   })
+   
+
    socket.on('createMessage',(message,callback)=>{
        console.log('got new message from client',message)
        //for broadcasting
        io.emit('newMessage',generateMessage( message.from,message.text))
+     
        callback(); 
    
    })
@@ -33,6 +51,11 @@ io.on('connection',(socket)=>{
    })
    socket.on('disconnect',()=>{
        console.log('user disconnected');
+       var user=users.removeUser(socket.id)
+       if(user){
+           io.to(user.room).emit('updateUserList',users.getUserList(user.room));
+           io.to(user.room).emit('newMessage',generateMessage('Admin',`${user.name} has left`));
+       }
    })
 })
 
